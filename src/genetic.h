facb2fec9e78db6ff1d929cd57553a44bafdb551
Rui Ferreira <ruiandreferreira@gmail.com>
2010-11-20 17:08:16 +0100

    updated documentation

--- src/genetic.h
+++ src/genetic.h
@@ -6,17 +6,18 @@
 #include <string.h>
 #include "life.h"
 
-
 #define GENESIZE 20
 #define POOLSIZE 200
 #define POS_X 0
 #define POS_Y 1
 
+/*	Gene structure that represents the initial
+	object that we are trying to optimize */
 typedef struct {
 	/* referential origin */
 	int x,y;
 
-	/* coordinates of the initial living cells*/
+	/* coordinates of the initial living cells, up to GENESIZE of them*/
 	int dna[GENESIZE][2];
 
 	/* the mutation rate is also coded in the gene so it can find the optimal one */
@@ -26,27 +27,39 @@ typedef struct {
 	int val;
 } gene;
 
+
+/* The pool of genes that is keept in memory
+   with each generation */
 typedef struct {
-	/* a pool of genes */
 	gene elements[POOLSIZE];
 } genepool;
 
+/*	Initialize and allocate a gene */
 gene * gene_new(void);
 
+/*	Crossover two genes */
 void gene_crossover(gene *ngene, gene *a, gene *b);
 
+/*	Output a gene */
 void gene_print(gene *g);
 
+/* Evaluate how good a gene is. This consists of running the game of life
+   for ngenerations and counting how many cells are occupied */
 void gene_eval(gene *g, int ngen);
 
+/* Compare the fitness of two genes */
 int gene_cmp(const void *a,const void *b);
 
+/* Random mutation of a gene */
 void gene_mutation(gene *g);
 
+/* Initialize and allocate a gene pool */
 genepool * genepool_new(void);
 
+/* Do the whole cycle of reproduction. Crossover + Mutation */
 void genepool_reproduce(genepool *p);
 
+/* The complete logic of the genetic algorithm */
 void genetic_algorithm(int ngenerations);
 
 #endif
--- src/life.h
+++ src/life.h
@@ -5,26 +5,31 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*	The width and height of the board
+	we don't expand infinitely but just
+ 	within the board margins
+ */
 #define BOARDWIDTH 20
 #define BOARDHEIGHT 20
 
+/*	Data structure that represents the board */
 typedef struct {
 	int board[BOARDWIDTH][BOARDHEIGHT];
 } lifegame;
 
-/* create a new board */
+/*	Create a new board */
 lifegame * lifegame_new(void);
 
-/* prints the board */
+/*	Prints the board */
 void lifegame_print(lifegame *l);
 
-/* run one step */
+/*	Run one step */
 void lifegame_step(lifegame *p);
 
-/* run for several generations */
+/*	Run for several generations */
 void lifegame_run(lifegame *l,int nsteps);
 
-/* count how many pos are occupied */
+/*	Count how many pos are occupied */
 int lifegame_eval(lifegame *l);
 
 #endif
